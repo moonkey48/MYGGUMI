a1a3c03e50c6cc1093c48dbaafd89d06e7c920cd
Moonkey <moonkey48@icloud.com>
2023-08-16 17:46:10 +0900

    [setting] start project

---
+++ MYGGUMI/MYGGUMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYGGUMIApp.swift
+//  MYGGUMI
+//
+//  Created by Seungui Moon on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct MYGGUMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
