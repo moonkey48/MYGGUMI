a22a976c6c9c831d76a227cf4c6c47bf4e9b2040
Moonkey <moonkey48@icloud.com>
2023-08-27 09:56:06 +0900

    feat - JobExperiencedView 구현 완료

--- MYGGUMI/Source/View/JobSpace/JobExperiencedView.swift
+++ MYGGUMI/Source/View/JobSpace/JobExperiencedView.swift
@@ -14,13 +14,13 @@ struct JobExperiencedView: View {
     @State private var cosinusB = cos(90 * Double.pi / 180)
     @State private var sinusC = sin(90.0 * Double.pi / 180)
     @State private var cosinusC = cos(90 * Double.pi / 180)
-    @State private var sinusD = sin(90.0 * Double.pi / 180)
-    @State private var cosinusD = cos(90 * Double.pi / 180)
     
     @State private var time: CGFloat = 90
     
     let circleWidth: CGFloat = 350
     let circleHeight: CGFloat = 250
+    let timeGapB: CGFloat = 180
+    let timeGapC: CGFloat = 240
     
     var body: some View {
         BackgroundView {
@@ -36,14 +36,19 @@ struct JobExperiencedView: View {
                 EllipseView
                 EllipseJobElement(text: "시각\n디자이너")
                     .offset(x:sinusA * circleWidth / 2, y: cosinusA * circleHeight / 2)
+                    .rotationEffect(Angle(degrees: -30))
                 EllipseJobElement(text: "디지털\n마케터")
-                    .offset(x:(sinusB + 0.5) * circleWidth / 2, y: cosinusB * circleHeight / 2)
+                    .offset(x:sinusB * circleWidth / 2, y: cosinusB * circleHeight / 2)
+                    .rotationEffect(Angle(degrees: -30))
+                EllipseJobElement(text: "작사가")
+                    .offset(x:sinusA * circleWidth, y: cosinusA * circleHeight)
+                    .rotationEffect(Angle(degrees: -30))
                 EllipseJobElement(text: "동화책\n디자이너")
                     .offset(x:sinusC * circleWidth, y: cosinusC * circleHeight)
+                    .rotationEffect(Angle(degrees: -30))
                 VStack {
                     Image("profile_noStroke")
                     ZStack {
-                        
                         Rectangle()
                             .fill(.white.opacity(0.3))
                             .blur(radius: 20)
@@ -62,7 +67,12 @@ struct JobExperiencedView: View {
                 time += 1
                 sinusA = sin(time * Double.pi / 180)
                 cosinusA = cos(time * Double.pi / 180)
-
+                let gapTimeB = time + timeGapB
+                sinusB = sin(gapTimeB * Double.pi / 180)
+                cosinusB = cos(gapTimeB * Double.pi / 180)
+                let gapTimeC = time + timeGapC
+                sinusC = sin(gapTimeC * Double.pi / 180)
+                cosinusC = cos(gapTimeC * Double.pi / 180)
             }
         }
     }
@@ -72,19 +82,49 @@ extension JobExperiencedView {
     var EllipseView: some View {
         ZStack {
             Ellipse()
-                .stroke(style: StrokeStyle(lineWidth: 1))
+                .stroke(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.purple,
+                        Color.white,
+                        Color.purple
+                        
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ),
+                lineWidth: 2)
                 .frame(width: circleWidth,height: circleHeight)
-                .rotationEffect(Angle(degrees: -25))
+                .rotationEffect(Angle(degrees: -30))
             Ellipse()
-                .stroke(style: StrokeStyle(lineWidth: 1))
+                .stroke(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.purple,
+                        Color.white,
+                        Color.purple
+                        
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ),
+                lineWidth: 2)
                 .frame(width: circleWidth * 1.4,height: circleHeight * 1.4)
-                .rotationEffect(Angle(degrees: -25))
-//            Ellipse()
-//                .stroke(style: StrokeStyle(lineWidth: 1))
-//                .frame(width: circleWidth * 2,height: circleHeight * 2)
-//                .rotationEffect(Angle(degrees: -25))
+                .rotationEffect(Angle(degrees: -30))
+            Ellipse()
+                .stroke(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.purple,
+                        Color.white,
+                        Color.purple
+                        
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ),
+                lineWidth: 2)
+                .frame(width: circleWidth * 2,height: circleHeight * 2)
+                .rotationEffect(Angle(degrees: -30))
         }
-        .blur(radius: 2)
+        .blur(radius: 1)
     }
 }
 struct EllipseJobElement: View {
@@ -95,11 +135,22 @@ struct EllipseJobElement: View {
                 .fill(.white.opacity(0.2))
                 .frame(width: 80, height: 80)
             Circle()
-                .stroke(style: StrokeStyle(lineWidth: 2))
+                .stroke(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.purple,
+                        Color.white,
+                        Color.purple
+                        
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ),
+                lineWidth: 2)
                 .frame(width: 80, height: 80)
             Text(text)
                 .description(.white, 15)
                 .multilineTextAlignment(.center)
+                .rotationEffect(Angle(degrees: 30))
         }
         .frame(width: 80, height: 80)
     }
