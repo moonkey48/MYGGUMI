0f6e13a3c61c8549860d9cba19632381875b1c04
Moonkey <moonkey48@icloud.com>
2023-08-28 12:24:03 +0900

    feat - JobSpaceView 기본 UI 구현

--- MYGGUMI/Source/View/JobSpace/JobSpaceView.swift
+++ MYGGUMI/Source/View/JobSpace/JobSpaceView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct JobSpaceView: View {
+    @State private var isShowExperience = false
     
     var body: some View {
-        Text("main space view")
+        BackgroundView {
+            VStack {
+                HeaderView()
+                toggleView
+                Text("다양한 감정을 초등학생에게\n아름답게 전달하는 사람")
+                    .description(.white, 19)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                CustomTabView()
+            }
+            ZStack {
+                ValueOrbit
+                CenterCircle
+            }
+        }
+    }
+}
+extension JobSpaceView {
+    var ValueOrbit: some View {
+        Ellipse()
+            .stroke(LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.purple,
+                    Color.white,
+                    Color.purple
+                    
+                ]),
+                startPoint: .leading,
+                endPoint: .trailing
+            ),
+            lineWidth: 2)
+            .frame(width: 850,height: 660)
+            .rotationEffect(Angle(degrees: -40))
+            .offset(x: -310, y: 150)
+    }
+    var CenterCircle: some View {
+        ZStack {
+            Circle()
+                .fill(.white.opacity(0.2))
+            Circle()
+                .stroke(LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.purple,
+                        Color.white,
+                        Color.purple
+                        
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ),
+                lineWidth: 2)
+            HStack {
+                Spacer()
+                Text("나의\n가치")
+                    .description(.white, 30)
+                    .multilineTextAlignment(.center)
+            }
+            .padding(50)
+        }
+        .frame(width: 260, height: 260)
+        .offset(x: -200, y: 90)
+        
+    }
+    var toggleView: some View {
+        HStack {
+            Spacer()
+            Button {
+               
+            } label: {
+                Image(isShowExperience ? "mission_toggle_on" : "mission_toggle_off")
+            }
+        }
+        .padding()
     }
 }
 
